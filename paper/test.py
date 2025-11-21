63c9f66e25ab5e29bf00fc43a4acc2a790a9baa4
Clouds Flowing <clouds.flowing@gmail.com>
2025-11-22 01:24:23 +0800

    add question2

--- paper/test.py
+++ paper/test.py
@@ -1,10 +1,50 @@
 # %%
-chr(0) # '\x00'
+c = chr(0)
+print(c) # '\x00'
+print(repr(c)) # "'\\x00'"
+print(f"<{c}>") # <>
 
 # %%
-repr(chr(0)) # "'\\x00'"
+test_string = "hello! こんにちは!"
+utf8_encoded = test_string.encode('utf-8')
+print(utf8_encoded) # b'hello! \xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf!'
+print(type(utf8_encoded)) # <class 'bytes'>
+print(list(utf8_encoded)) # [104, 101, 108, 108, 111, 33, 32, 227, 129, 147, 227, 130, 147, 227, 129, 171, 227, 129, 161, 227, 129, 175, 33]
+print(len(test_string)) # 13
+print(len(utf8_encoded)) # 23
 
 # %%
-print(f"<{chr(0)}>") # <>
+utf16_encoded = test_string.encode('utf-16')
+print(utf16_encoded) # b'\xff\xfeh\x00e\x00l\x00l\x00o\x00!\x00 \x00S0\x930k0a0o0!\x00'
+print(list(utf16_encoded)) # [255, 254, 104, 0, 101, 0, 108, 0, 108, 0, 111, 0, 33, 0, 32, 0, 83, 48, 147, 48, 107, 48, 97, 48, 111, 48, 33, 0]
+print(len(utf16_encoded)) # 28
+
+# %%
+utf32_encoded = test_string.encode('utf-32')
+print(utf32_encoded) # b'\xff\xfe\x00\x00h\x00\x00\x00e\x00\x00\x00l\x00\x00\x00l\x00\x00\x00o\x00\x00\x00!\x00\x00\x00 \x00\x00\x00S0\x00\x00\x930\x00\x00k0\x00\x00a0\x00\x00o0\x00\x00!\x00\x00\x00'
+print(list(utf32_encoded)) # [255, 254, 0, 0, 104, 0, 0, 0, 101, 0, 0, 0, 108, 0, 0, 0, 108, 0, 0, 0, 111, 0, 0, 0, 33, 0, 0, 0, 32, 0, 0, 0, 83, 48, 0, 0, 147, 48, 0, 0, 107, 48, 0, 0, 97, 48, 0, 0, 111, 48, 0, 0, 33, 0, 0, 0]
+print(len(utf32_encoded)) # 56
+
+# %%
+def decode_utf8_bytes_to_str_wrong(bytestring: bytes):
+  return "".join([bytes([b]).decode("utf-8") for b in bytestring])
+
+decode_utf8_bytes_to_str_wrong("hello".encode("utf-8")) # 'hello'
+
+# %%
+try:
+  decode_utf8_bytes_to_str_wrong("你好".encode("utf-8"))
+except UnicodeDecodeError:
+  pass
+
+# %%
+def cannot_decode(bytestring: bytes):
+  try:
+    return bytestring.decode('utf-8')
+  except UnicodeDecodeError:
+    print(f"cannot decode {bytestring}")
+
+cannot_decode(b'\xff\x00')
+cannot_decode(b'\xff')
 
 # %%
--- paper/writeup.typ
+++ paper/writeup.typ
@@ -1,4 +1,6 @@
-== Problem (unicode1)
+= BPE Tokenizer
+
+== Problem (unicode1): Understanding Unicode
 === a) What Unicode character does `chr(0)` return?
 It would return `'\x00'`.
 
@@ -7,3 +9,17 @@ The repr of `chr(0)` would be `'\\x00'`, while `print(chr(0))` would produce no 
 
 === c) What happens when this character occurs in text?
 When this character occurs in text, it is visibly represented as a space.
+
+== Problem (unicode2): Unicode Encodings
+=== a) What are some reasons to prefer training our tokenizer on UTF-8 encoded bytes, rather than UTF-16 or UTF-32?
+The raw bytes of UTF-8 are more compact, as it uses a variable-length encoding scheme that is more efficient for common characters. UTF-8 is also backward compatible with ASCII, making it easier to handle in many systems and applications.
+
+=== b) Consider the following (incorrect) function, which is intended to decode a UTF-8 byte string into a Unicode string. Why is this function incorrect?
+```python
+def decode_utf8_bytes_to_str_wrong(bytestring: bytes):
+  return "".join([bytes([b]).decode("utf-8") for b in bytestring])
+```
+`decode_utf8_bytes_to_str_wrong("你好".encode("utf-8"))` would throw `UnicodeDecodeError`
+
+=== c) Give a two byte sequence that does not decode to any Unicode character(s).
+`b'\xff\x00'` and `b'\xff'`, since according to utf-8, any byte starting with `0xff` is invalid.
