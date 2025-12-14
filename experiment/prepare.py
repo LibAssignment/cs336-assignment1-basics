23a4a1d9fd9d46984c8bd4f79f6aeff3101268cc
Clouds Flowing <clouds.flowing@gmail.com>
2025-12-15 03:43:49 +0800

    add unitoken

--- experiment/prepare.py
+++ experiment/prepare.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 Path("logs").mkdir(parents=True, exist_ok=True)
 logging.basicConfig(filename=f"logs/prepare-{int(time.time())}.log")
-fixture_folder = Path(__file__).parent / "fixtures"
+fixture_folder = Path(__file__).parent.parent / "data/fixtures"
 
 fixture_url = [
   "https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-train.txt",
@@ -70,4 +70,41 @@ for i in fixture_url:
     continue
   _download(i, filename)
 
+# %%
+from pathlib import Path
+fixture_folder = Path(__file__).parent.parent / "tests/fixtures"
+
+# %%
+from unitoken import PreTokenizer, BpeTrainer
+from pathlib import Path
+tokenizer_dir = Path(__file__).parent.parent / "data/tokens"
+
+def create_tokenizer(name: str, input_dir: Path = fixture_folder, output_dir: Path = tokenizer_dir, special_tokens: list[str] | None = None, vocab_size: int = 10000):
+  input_file = input_dir / f"{name}.txt"
+  if special_tokens is None:
+    special_tokens = ["<|endoftext|>"]
+  words = PreTokenizer(special_tokens, None).get_words_from_file(input_file, 1024)
+  trainer = BpeTrainer(special_tokens)
+  trainer.add_words(words)
+  trainer.train(vocab_size)
+  output_dir.mkdir(parents=True, exist_ok=True)
+  trainer.save(name, outdir=output_dir)
+
+create_tokenizer("tinystories_sample_5M", vocab_size=1000)
+# %%
+from unitoken import BpeEncoder
+import numpy as np
+def create_idx_file(name: str, input_dir: Path = fixture_folder, output_dir: Path = tokenizer_dir, chunks: int = 1024):
+  input_file = input_dir / f"{name}.txt"
+  encoder = BpeEncoder.load(name, input_dir=output_dir)
+  idxs = encoder.encode_file(input_file, chunks)
+  np.save(output_dir/f"idx.{name}.npy", idxs)
+  if "train" in name:
+    name_valid = name.replace("train", "valid")
+    valid_file = input_dir / name_valid
+    if valid_file.exists():
+      valid_idxs = encoder.encode_file(valid_file, chunks)
+      np.save(output_dir/f"idx.{name_valid}.npy", valid_idxs)
+
+create_idx_file("tinystories_sample_5M")
 # %%
